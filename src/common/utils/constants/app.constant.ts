617103ca3439dffe940596c4b06885d579d43460
TuHungg <tuhoanghaihung@gmail.com>
2022-07-16 14:17:03 +0700

    update cors

--- src/common/utils/constants/app.constant.ts
+++ src/common/utils/constants/app.constant.ts
@@ -8,4 +8,8 @@ export const ADMIN_ROLE_ID = '622a1be2a84b6d2e216a9a89';
 export const TIME_ZONE = 'Asia/Ho_Chi_Minh';
 
 // CLIENT WHITELIST
-export const CLIENT_WHITELIST = ['http://localhost:5000', 'https://onlinecourse-hsu.vercel.app'];
+export const CLIENT_WHITELIST = [
+    'http://localhost:5000',
+    'https://onlinecourse-hsu.vercel.app',
+    'https://onlinecourse-gamma.vercel.app',
+];
